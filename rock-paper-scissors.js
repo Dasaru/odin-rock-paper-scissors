4e64a0f7e41de1414cad56b9c3db805fb0244da6
Dasaru <dberry02jr@yahoo.com>
2023-11-14 15:35:52 -0800

    Add computer generated choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,17 @@
 window.onload = function() {
     console.log("Page loaded!");
 }
+
+function getComputerChoice() {
+    let rand = Math.floor(Math.random()*3);
+    switch (rand) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+        default:
+            throw Error("Impossible value returned.");
+    }
+}

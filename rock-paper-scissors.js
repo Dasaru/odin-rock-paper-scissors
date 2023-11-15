72bb61f1bb4c98c35cfb6391e5985601d7d33f87
Dasaru <dberry02jr@yahoo.com>
2023-11-14 18:25:35 -0800

    Add full game functionality
    Adds the ability to play a series of games in a a row.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     console.log("Page loaded!");
+    game();
 }
 
 function getComputerChoice() {
@@ -23,16 +24,68 @@ function playRound(playerSelection, computerSelection) {
         throw Error("Bad player input. Values should be Rock, Paper, or Scissors.");
     }
     if (playerSelection === computerSelection) {
-        return `Game is a tie! Both players played ${playerSelection}`;
+        console.log(`Game is a tie! Both players played ${playerSelection}`);
+        return 0;
     } else if (
         (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
         (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
         (playerSelection === "PAPER" && computerSelection === "ROCK")
     ) {
         // Player Wins
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        return 1;
     } else {
         // Player loses
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        return -1;
+    }
+}
+
+function isValidChoice(choice) {
+    choice = choice.toUpperCase();
+    return (
+        choice === "ROCK" ||
+        choice === "PAPER" ||
+        choice === "SCISSORS"
+    );
+}
+
+function game() {
+    let gamesPlayed = 0;
+    let playerWins = 0;
+    let computerWins = 0;
+
+    while (gamesPlayed < 5){
+
+        // Get player input
+        let playerChoice = prompt("Type either Rock, Paper, or Scissors.");
+        console.log(`You picked ${playerChoice}`);
+
+        // Valid player input
+        if (isValidChoice(playerChoice)) {
+            // Generate random computer choice.
+            let computerChoice = getComputerChoice();
+            // Play the round
+            let result = playRound(playerChoice, computerChoice);
+            // If there is a winner, increment the winner.
+            if (result === 1) playerWins++;
+            if (result === -1) computerWins++;
+            // Increment the games played counter
+            gamesPlayed++;
+            
+        } else {
+            // Invalid player input
+            console.log("Invalid input. Please input Rock, Paper, or Scissors.");
+        }
+
+    }
+
+    console.log(`The player has won ${playerWins} times and the computer has won ${computerWins} times.`);
+    if (playerWins > computerWins) {
+        console.log(`You win the series!`);
+    } else if (playerWins < computerWins) {
+        console.log(`You lose the series...`);
+    } else {
+        console.log(`The series is a tie.`);
     }
 }
